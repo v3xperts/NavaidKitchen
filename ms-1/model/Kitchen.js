d343713da0c330de01aeac5eaf85dfbf86628cce
unknown <ankur@v3xperts.com>
2018-04-02 17:30:16 +0530

    update

--- ms-1/model/Kitchen.js
+++ ms-1/model/Kitchen.js
@@ -52,7 +52,7 @@ var KitchenSchema = new Schema({
  bankinginformation : Array,
  tax: {status : "", value : ""},
  serviceAllow: {"daliymenuservice": {type: Boolean, default: true}, "cateringservice": {type: Boolean, default: false}, "mealpackageservice": {type: Boolean, default: false}, "comboservice": {type: Boolean, default: true}},
- completeprofile : {"restaurantbasic" : {type: Number, dafault : 0}, "kitchenservice" : {type: Number, dafault : 0}}
+ completeprofile : {type: Number, dafault : 0}
  });
 
 // the schema is useless so far
