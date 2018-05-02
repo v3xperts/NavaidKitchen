2980da0b6bd4bb4e55d25d879623196ee1c1613f
unknown <business@v3xperts.com>
2018-05-02 13:22:40 +0530

    update

--- ms-4/model/Order.js
+++ ms-4/model/Order.js
@@ -25,7 +25,7 @@ status : {type : String, default : 'received'},
 created_at: { type: Date },
 cardPaidStatus: Object,
 timezone: String,
-menuStatus: {type: String , default: false},
+menuStatus: {type: Boolean , default: false},
 currency: String
 });
 
