a8ed96a226aa7e0f0d91051196d6927ead76317c
v3service7 <manvi@v3xperts.com>
2018-09-20 22:54:08 +0530

    api update

--- ms-3/routes/index.js
+++ ms-3/routes/index.js
@@ -196,10 +196,6 @@ router.post('/customer-address/:id',function(req, res){
   var response = {};
   var fullAddress = req.body.address+" "+req.body.zipcode+" "+req.body.city+" "+req.body.country;
   geocoder.geocode(fullAddress, function(err, gResponse) {
-    
-    console.log("err, gResponse");
-    console.log(err, gResponse);
-
     if(err){
       response = {"error" : true,"message" : err};
       res.json(response);
