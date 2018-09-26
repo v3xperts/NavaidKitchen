6d6c4a2e190bd787e678cc25885519d7f4864061
v3service7 <manvi@v3xperts.com>
2018-09-26 22:07:43 +0530

    apiUpdate

--- ms-1/model/Partner.js
+++ ms-1/model/Partner.js
@@ -7,7 +7,7 @@ var OwnerSchema = new Schema({
   ownerfirstname: String,
   ownerlastname: String,  
   owneraddress : String,
-  userType : 'partner',
+  userType : { type: String, default: 'partner'},
   ownerphoneno : String,
   username: { type: String, lowercase: true, required: true, unique: true },
   email: { type: String, lowercase: true, required: true, unique: true },
